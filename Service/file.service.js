9108dd8d5a8863ec25a60ed75f62d4a5903e0fab
Meyazhagan <meyazhagan.ofcl@gmail.com>
2021-10-18 20:02:26 +0530

    made changes

--- Service/file.service.js
+++ Service/file.service.js
@@ -10,31 +10,31 @@ const getPath = (filename) => {
 };
 
 module.exports = {
-  getOne: async (req, res) => {
+  getOne: (req, res) => {
     let filename = req.params.filename;
     //reading a file
-    await fs.readFile(getPath(filename), (err, file) => {
+    fs.readFile(getPath(filename), (err, file) => {
       if (err) return res.status(404).send({ message: "No Such File Found" });
       res.send({ filename, content: file.toString("utf8"), data: file });
     });
   },
 
-  getAll: async (req, res) => {
+  getAll: (req, res) => {
     //reading all filename
-    await fs.readdir("file", (err, files) => {
+    fs.readdir("file", (err, files) => {
       if (err) return res.status(400).send(err.message);
       res.send({ files });
     });
   },
 
-  post: async (req, res) => {
+  post: (req, res) => {
     const timestamp = Date.now();
     const now = new Date(timestamp);
     const formated = date.format(now, "YYYY-MM-DD HH:mm:ss");
     const filename = date.format(now, "YYYYMMDD-HHmmss");
     const content = `${timestamp} - ${formated}`;
     // creating new file with current timestamp
-    await fs.writeFile(getPath(filename), content, (err) => {
+    fs.writeFile(getPath(filename), content, (err) => {
       if (err) return res.status(400).send(err.message);
       res.send({ message: "File Created", filename: `${filename}.txt` });
     });
