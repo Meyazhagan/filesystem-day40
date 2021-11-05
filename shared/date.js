d36a2611ab96b28d9c15e8a0eda90e2dce5add97
Meyazhagan <meyazhagan.ofcl@gmail.com>
2021-11-05 19:05:50 +0530

    updated time zone

--- shared/date.js
+++ shared/date.js
@@ -5,7 +5,7 @@ module.exports.getTimeContent = () => {
   const contentPattern = "T - dd/MM/yyyy HH:mm:ss (z)";
   const logPattern = "dd/MM/yyyy HH:mm:ss (z)";
   const dateTimePattern = "dd_MM_yyyy-HH_mm_ss";
-  const content = format(zonedDate, {
+  const content = format(zonedDate, contentPattern, {
     timeZone: "Asia/Kolkata",
   });
   const dateTime = format(zonedDate, dateTimePattern, {
